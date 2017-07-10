517eaf52fb29e741fa06c97c4d28c345431ec0f0
Matt Adams <mattadams@factor1studios.com>
2017-07-09 20:41:55 -0700

    Merged in a few parts from 2016

---
+++ parts/sponsors.php
@@ -0,0 +1,251 @@
+<!--
+Title Sponsor - Limit 1
+Premier Sponsor - Limit 5
+Premier Print and Large Format Sponsor - Limit 5
+Leadership Sponsor 
+Industry Sponsor
+Agency Sponsor
+-->
+
+
+
+
+
+<?php // Sponsors Container ?>
+<section class="sponsors--container">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h2 class="text-teal">
+          PHXDW is made possible by generous sponsors
+        </h2>
+      </div>
+    </div>
+
+    <?php // Title Sponsors ?>
+    <?php if(have_rows('title_sponsors', 'option')):?>
+    
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Premier Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('title_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+      
+    <?php endif;?>
+
+
+
+
+
+    <?php // Premier Sponsors ?>
+    <?php if(have_rows('premier_sponsors', 'option')):?>
+    
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Premier Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('premier_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+    
+
+    <?php // Print Sponsors ?>
+    <?php if(have_rows('print_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Premier Print And Large Format Sponsor
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('print_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+
+
+    <?php // Leadership Sponsors ?>
+    <?php if(have_rows('leadership_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Leadership Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('leadership_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+    
+
+
+
+    <?php // Industry Sponsors ?>
+    <?php if(have_rows('industry_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Industry Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('industry_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+    
+    <?php // Agency Sponsors ?>
+    <?php if(have_rows('agency_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+          Agency Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('agency_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+    
+
+    
+    
+     <?php // Other Sponors ?>
+    
+    <?php if(have_rows('other_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+           Other Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('other_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+     <?php // Student  Sponsors ?>
+    
+    <?php if(have_rows('student_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+           Student Scholarship Sponsors
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('student_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+        <?php // Partner Organization / Sponsors ?>
+    
+    <?php if(have_rows('partner_sponsors', 'option')):?>
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="sponsors--headline">
+           Partner Organizations Who Helped Organize This Event
+        </h4>
+      </div>
+    </div>
+    
+      <div class="row row--justify-content-center">
+        <?php while(have_rows('partner_sponsors', 'option')): the_row();?>
+          <div class="col-3 text-center sponsors--grid-item">
+            <a href="<?php the_sub_field('sponsor_link');?>" target="_blank">
+              <img src="<?php the_sub_field('sponsor_image');?>" alt="Sponsor Image">
+            </a>
+          </div>
+        <?php endwhile;?>
+      </div>
+    <?php endif;?>
+    
+    
+
+  </div>
+</section>
--- parts/tickets.php
+++ parts/tickets.php
@@ -1,68 +1,38 @@
-<section class="panel panel--lime tickets container">
-	<div class="row">
-		<div class="col-6 col-centered">
-			<img src="<?php bloginfo('template_url');?>/assets/images/Spider.gif" class="glitch" width="300" height="300" />
-		</div>
-	</div>
-	
-	
-	<div class="row">
-		<div class="col-6">
-			<img src="<?php bloginfo('template_url');?>/assets/images/text_earlybird.png" class="text_earlybird" width="346" height="133" />
-			<br><br>
-			<h4>Method + Madness Conference
-				<br>
-				October 28 - 29, 2017</h4>
-				<p>Conference tickets include admission to the PHXDW Kickoff Party on Friday, October 27, the Saturday evening reception on October 28 and the PHXDW Closing Party on November 3rd.
-</p>
-		</div>
-		
-		<div class="col-6">
-			
-			<h4>GET YOUR EARLY BIRD TICKET NOW AND SAVE!</h4>
-			<p style="margin-bottom:40px;">Only 100 Early Bird tickets are available, so grab your spot now and save up to 60% off the price of a full-price ticket! And, as an extra bonus for our most loyal PHXDW fans, you also get a FREE conference t-shirt with your Early Bird ticket! See order form for details.
-</p>
-			
-			<div class="pricing-table">
-				<div class="row row--full-width">
-					<div class="col-5 sm-col-5"><p> AIGA Member</p></div>
-					<div class="col-3 sm-col-3 text-left"><p> <strong> $159 </strong></p></div>
-					<div class="col-4 sm-col-4 text-left"><p> <strike> $299 full price </strike></p></div>
-				</div>
-				
-				<div class="row row--full-width">
-					<div class="col-5 sm-col-5"><p> Non-Member</p></div>
-					<div class="col-3 sm-col-3 text-left"><p> <strong> $239 </strong></p></div>
-					<div class="col-4 sm-col-4 text-left"><p> <strike> $379 full price </strike></p></div>
-				</div>
-					
-				<div class="row row--full-width">
-					<div class="col-5 sm-col-5"><p> Student Member</p></div>
-					<div class="col-3 sm-col-3 text-left"><p> <strong> $79 </strong></p></div>
-					<div class="col-4 sm-col-4 text-left"><p> <strike> $99 full price </strike></p></div>
+<div class="row" style="margin: 40px auto;">
+				<div class="col-3 priceblock first stretch">
+					<div class="content">
+					<p class="title">AIGA Member<br>General Admission</p>
+					<p class="price">$299</p>
 					</div>
-					
-				<div class="row row--full-width">
-					<div class="col-5 sm-col-5"><p> Student</p></div>
-					<div class="col-3 sm-col-3 text-left"><p> <strong> $99 </strong></p></div>
-					<div class="col-4 sm-col-4 text-left"><p> <strike> $149 full price </strike></p></div>
 				</div>
-				
-				<div class="row row--full-width">
-					<div class="col-5 sm-col-5"><p> Faculty Member</p></div>
-					<div class="col-3 sm-col-3 text-left"><p> <strong> $79 </strong></p></div>
-					<div class="col-4 sm-col-4 text-left"><p> <strike> $99 full price </strike></p></div>
+
+				<div class="col-3 priceblock second stretch">
+					<div class="content">
+					<p class="title">AIGA Non-Member<br>General Admission</p>
+					<p class="price">$369</p>
+					</div>
 				</div>
-				
-				<div class="row row--full-width">
-					<div class="col-5 sm-col-5"><p> Faculty</p></div>
-					<div class="col-3 sm-col-3 text-left"><p> <strong> $99 </strong></p></div>
-					<div class="col-4 sm-col-4 text-left"><p> <strike> $149 full price </strike></p></div>
+
+				<div class="col-3 priceblock third stretch">
+					<div class="content">
+					<p class="title">AIGA Member<br>Student Admission</p>
+					<p class="price">$99</p>
+					</div>
 				</div>
-				
-				<a href="https://phxdwmethodmadness2017.eventbrite.com/" class="button light-text">Buy Tickets</a>
+
+				<div class="col-3 priceblock fourth stretch">
+					<div class="content">
+					<p class="title">AIGA Non-Member<br>Student Admission</p>
+					<p class="price">$149</p>
+					</div>
+				</div>
+
 			</div>
-			
-		</div>
-	</div>
-</section>
+
+
+			<div class="row">
+				<div class="col-4 col-centered">
+					<a href="https://www.eventbrite.com/e/method-madness-conference-phxdw-2016-tickets-24939107542" class="button white text-pink text-center" style="margin-bottom: 40px; width:100%;">
+						GET YOUR TICKET</a>
+				</div>
+			</div>
