4aa3c9572e0e38c448e481216853f55524ea31da
Matt Adams <mattadams@factor1studios.com>
2017-09-10 10:40:14 -0700

    Update ticket messaging on speakers

--- single-f1_staffgrid_cpt.php
+++ single-f1_staffgrid_cpt.php
@@ -131,9 +131,7 @@
 			</h2>
 
 			<p class="text-white text-center">
-				All tickets include entrance to the Method + Madness Conference, PHXDW Kickoff Party,
-				Method + Madness 	Opening Reception, and the PHXDW Closing Party
-			</p>
+				All two-day tickets include entrance to the Method + Madness Conference, PHXDW 2017 Kickoff Party, Method + Madness Evening Reception, and the PHXDW 2017 Closing Party. One-day M+M conference passes, group and AIGA discount opportunities are also available at the tickets link. 			</p>
 
 			<?php
 				//============ ** ============ //
