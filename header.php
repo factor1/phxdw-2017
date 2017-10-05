210274d6c2b18c6ab23945bcfa57b0f40a33ee82
Matt Adams <mattadams@factor1studios.com>
2017-10-05 14:14:57 -0700

    Unheap and nav width updates

--- header.php
+++ header.php
@@ -25,6 +25,10 @@
 	  
 	  <script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');__gaTracker('create','UA-64160014-1','auto');__gaTracker('set','forceSSL',true);__gaTracker('send','pageview');</script>
   
+  <script type="text/javascript">
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+      heap.load("1643074858");
+</script>
   
 </head>
 
@@ -54,10 +58,10 @@
 <?php }
 else {
 ?>
-		  <div class="md-col-4 sm-col-8 header--branding">
+		  <div class="md-col-3 sm-col-8 header--branding">
 			  <h1><a href="<?php bloginfo( 'home' ); ?>">PHX Design Week 2017</a></h1>
 		  </div>
-		  <div class="col-8 md-hide-down">
+		  <div class="col-9 md-hide-down">
 			  <a href="https://phxdwmethodmadness2017.eventbrite.com/" class="button header--buynow">Get Tickets</a>
 			  <?php wp_nav_menu(array ('menu' => 'primary', 'depth'=>2, 'container_id' => 'interior-menu')); ?>
 		  </div>
