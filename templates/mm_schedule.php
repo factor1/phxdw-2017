36d4e734050445adb45ed382712a5d9b3ddcdc4f
Dana <dana@factor1studios.com>
2017-10-09 09:02:06 -0700

    restructure modal to only display and function when id is provided in ACF

--- templates/mm_schedule.php
+++ templates/mm_schedule.php
@@ -100,7 +100,11 @@
 				<span class="cd-date"><?php the_sub_field('time'); ?></span>
 				<?php the_sub_field('detail'); ?>
 
-				<?php if( have_rows('breakouts') ) : while( have_rows('breakouts') ): the_row();?>
+				<?php if( have_rows('breakouts') ) : while( have_rows('breakouts') ): the_row();
+					// vars
+					$modal_id = get_sub_field('id');
+					$talk_title = get_sub_field('talk_title');
+				?>
 							<div class="breakout">
 
 								<p class="tag"><?php the_sub_field('tag'); ?></p>
@@ -108,12 +112,18 @@
 									<p class="room"><?php the_sub_field('room'); ?></p>
 									<p class="speaker"><?php the_sub_field('speaker'); ?><span>&nbsp;
 										<?php the_sub_field('company'); ?></span></p>
-									<p class="title"><a href="#<?php the_sub_field('id'); ?>"><?php the_sub_field('talk_title'); ?></a></p>
-									<div class="remodal" data-remodal-id="<?php the_sub_field('id'); ?>">
-									  <button data-remodal-action="close" class="remodal-close"></button>
-									  <h1>Remodal</h1>
-									 	<?php the_sub_field('details'); ?>
-									</div>
+									<p class="title">
+										<?php if($modal_id) : ?><a href="#<?php echo $modal_id; ?>"><?php endif; ?>
+											<?php echo $talk_title; ?>
+										<?php if($modal_id) : ?></a><?php endif; ?>
+									</p>
+									<?php if($modal_id) : ?>
+										<div class="remodal" data-remodal-id="<?php echo $modal_id; ?>">
+										  <button data-remodal-action="close" class="remodal-close"></button>
+										  <h2><?php echo $talk_title; ?></h2>
+										 	<?php the_sub_field('details'); ?>
+										</div>
+									<?php endif; ?>
 								</div>
 							</div>
 						<?php endwhile; endif; ?>
