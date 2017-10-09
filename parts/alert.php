ed6ec2226469b8bf2ab8f0afb182f3b001089c17
Dana <dana@factor1studios.com>
2017-10-09 10:20:42 -0700

    alert button now inline with text

--- parts/alert.php
+++ parts/alert.php
@@ -2,8 +2,10 @@
   <section class="alert-bar">
   	<div class="row">
   		<div class="col-12 col-centered text-center">
-  			<p><?php the_field('alert_message', 'options')?></p>
-        <a href="<?php the_field('alert_button_link', 'options')?>" class="button"><?php the_field('alert_button_text', 'options')?></a>
+  			<p>
+          <?php the_field('alert_message', 'options')?>
+          <a href="<?php the_field('alert_button_link', 'options')?>" class="button"><?php the_field('alert_button_text', 'options')?></a>
+        </p>
   		</div>
   	</div>
   </section>
