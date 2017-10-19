d22dfb38fd2f1d7a564cef615192678f390a97ee
Matt Adams <mattadams@factor1studios.com>
2017-10-18 18:38:18 -0700

    make that button black all the time.

--- parts/alert.php
+++ parts/alert.php
@@ -4,7 +4,7 @@
   		<div class="col-12 col-centered text-center">
   			<div class="alert-message">
           <?php the_field('alert_message', 'options')?>
-          <a href="<?php the_field('alert_button_link', 'options')?>" class="button black" style="font-size:16px;"><?php the_field('alert_button_text', 'options')?></a>
+          <a href="<?php the_field('alert_button_link', 'options')?>" class="button black" style="font-size:16px; background-color: #111"><?php the_field('alert_button_text', 'options')?></a>
         </div>
   		</div>
   	</div>
