f7b8cc9b72d054d768dc775bc2e383d66939a2bb
Matt Adams <mattadams@factor1studios.com>
2017-10-18 18:32:45 -0700

    Roll in kyles alert bar changes

--- parts/alert.php
+++ parts/alert.php
@@ -1,10 +1,10 @@
-<?php if(get_field('show_alert_bar', 'options') === true ) { ?>
+<?php if(get_field('show_alert_bar', 'options') === true && !is_page('events') && !is_singular('event') ) { ?>
   <section class="alert-bar container">
   	<div class="row" style="max-width:none;">
   		<div class="col-12 col-centered text-center">
   			<div class="alert-message">
           <?php the_field('alert_message', 'options')?>
-          <a href="<?php the_field('alert_button_link', 'options')?>" class="button" style="font-size:16px;"><?php the_field('alert_button_text', 'options')?></a>
+          <a href="<?php the_field('alert_button_link', 'options')?>" class="button black" style="font-size:16px;"><?php the_field('alert_button_text', 'options')?></a>
         </div>
   		</div>
   	</div>
